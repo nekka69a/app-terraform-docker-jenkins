ee6801a1bdac5ffbca88dd720ed25ad27d906af5
Nekka Amina <123459087+nekka69a@users.noreply.github.com>
2023-06-16 12:26:50 +0200

    Update providers.tf

--- Prod/providers.tf
+++ Prod/providers.tf
@@ -7,8 +7,8 @@ terraform {
 
 provider "azurerm" {
    subscription_id = var.subscription_id
-   #client_id = var.client_id
-   #client_secret = var.client_secret
+   client_id = var.client_id
+   client_secret = var.client_secret
    tenant_id = var.tenant_id
    skip_provider_registration = true
    features {}
