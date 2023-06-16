ad955d04478fe591c107090f07041dda3597416a
Nekka Amina <123459087+nekka69a@users.noreply.github.com>
2023-06-16 12:17:11 +0200

    Update variables.tf

--- Prod/variables.tf
+++ Prod/variables.tf
@@ -9,3 +9,13 @@ variable "tenant_id" {
     default = "7349d3b2-951f-41be-877e-d8ccd9f3e73c"
   
 }
+
+variable "client_id" {
+   description = "Azure Client ID"
+   default = "83c663ad-6bc8-4ef4-b733-ee5df94f5590"
+}
+
+variable "client_secret" {
+   description = "Azure Client Secret"
+   default = "UOi8Q~VCUtGkJGcL_6CgDxonx-UPr2ZCi032iaSI"
+}
